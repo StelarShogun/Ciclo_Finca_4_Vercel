97e1e490307c1010c8043fb207533c3ee808bf36
Dilan Rodríguez <129096951+StelarShogun@users.noreply.github.com>
2026-03-17 10:47:26 -0600

    Add reCAPTCHA configuration to services.php

--- config/services.php
+++ config/services.php
@@ -34,7 +34,6 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
@@ -47,4 +46,8 @@ return [
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
     ],
 
+    'recaptcha' =>  [
+    'key' => env('CAPTCHA_SITE_KEY'),
+    'secret' => env('CAPTCHA_SECRET_KEY'),
+ ],
 ];
