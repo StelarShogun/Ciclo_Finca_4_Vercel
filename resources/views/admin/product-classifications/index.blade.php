237d8403024e7ddba07c105d3a1149b2755b3ee2
Dilan Josue Rodríguez Cortes <raichualola12345678910@gmail.com>
2026-05-20 13:29:41 -0600

    fix vite error
    causing clasificaiones por producto fallar

--- resources/views/admin/product-classifications/index.blade.php
+++ resources/views/admin/product-classifications/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Características por producto - Ciclo Finca 4 Admin</title>
-    @vite(['resources/css/admin/suppliers/suppliers.css', 'resources/js/shared/ajax-pagination.js'])
+    @vite(['resources/css/admin/suppliers/suppliers.css', 'resources/js/admin/product-classifications/index.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
